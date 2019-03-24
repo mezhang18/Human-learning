caa35f33554a4fb408f18e65e5175e1af8bbe93c
shawnzhang12 <48869638+shawnzhang12@users.noreply.github.com>
2019-03-24 01:16:57 -0400

    Add files via upload

---
+++ Anime_Recomm.py
@@ -0,0 +1,231 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import csv
+from mpl_toolkits.mplot3d import Axes3D 
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+
+#%matplotlib inline 
+plt.rcParams['figure.figsize'] = (6,4)
+plt.style.use('ggplot')
+#%config InlineBackend.figure_formats = {'png', 'retina'}
+
+anime = pd.read_csv('./anime.csv')
+print(type(anime))
+anime.head(10)
+print(anime.head(10))
+print(anime.shape)
+
+user = pd.read_csv('./rating.csv')
+print(user.head(10))
+print(user.shape) 
+
+total= 0
+count = 0
+print(user[user['user_id']==1].rating.mean())
+
+MRPU = user.groupby(['user_id']).mean().reset_index()
+MRPU['mean_rating'] = MRPU['rating']
+MRPU.drop(['anime_id','rating'],axis=1,inplace=True)
+print(MRPU.head(10))
+
+'''
+with open('./rating.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    userid = []
+    for row in reader:
+        r = float(row['rating'])
+        if r != -1:
+            userid.append([r])
+print(userid[0:100])
+''' 
+
+user = pd.merge(user,MRPU,on=['user_id','user_id'])
+print(user.head(5))
+user = user.drop(user[user.rating < user.mean_rating].index)
+
+#Example of removing all ratings less than mean rating
+user[user['user_id']== 5].head(10)
+print('user.shape: '),
+print(user.shape)
+
+print('user["user_id"].unique(): '),
+print(user["user_id"].unique())
+
+#Renames user
+user = user.rename({'rating':'userRating'}, axis='columns')
+
+# merge 2 dataset
+mergedata = pd.merge(anime,user,on=['anime_id','anime_id'])
+mergedata= mergedata[mergedata.user_id <= 15000]
+mergedata.head(10)
+
+#Prints recovered unique anime ids
+print(len(mergedata['anime_id'].unique()))
+#Prints original list of anime ids
+print(len(anime['anime_id'].unique()))
+
+#Shows detail of anime which each user likes
+user_anime = pd.crosstab(mergedata['user_id'],mergedata['name'])
+#print(user_anime.head(3))
+
+pca = PCA(n_components=3)
+pca.fit(user_anime)
+pca_samples=pca.transform(user_anime)
+
+ps = pd.DataFrame(pca_samples)
+print(ps.head(5))
+
+tocluster = pd.DataFrame(ps[[0,1,2]])
+plt.rcParams['figure.figsize']=(16,9)
+'''fig = plt.figure()
+ax=Axes3D(fig)
+ax.scatter(tocluster[0],tocluster[2],tocluster[1])
+plt.title('Data points in 3D PCA axis', fontsize=20)
+plt.show()
+'''
+scores = []
+inertia_list = np.empty(8)
+for i in range(2,8):
+   kmeans = KMeans(n_clusters=i)
+   kmeans.fit(tocluster)
+   inertia_list[i] = kmeans.inertia_
+   scores.append(silhouette_score(tocluster, kmeans.labels_))
+plt.plot(range(0,8),inertia_list,'-o')
+plt.xlabel('Number of cluster')
+plt.axvline(x=4, color='blue', linestyle='--')
+plt.ylabel('Inertia')
+plt.show()
+
+
+plt.plot(range(2,8), scores);
+plt.title('Results KMeans')
+plt.xlabel('n_clusters');
+plt.axvline(x=4, color='blue', linestyle='--')
+plt.ylabel('Silhouette Score');
+plt.show()
+
+clusterer = KMeans(n_clusters=4,random_state=30).fit(tocluster)
+centers = clusterer.cluster_centers_
+c_preds = clusterer.predict(tocluster)
+
+print(centers)
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(tocluster[0], tocluster[2], tocluster[1], c = c_preds)
+plt.title('Data points in 3D PCA axis', fontsize=20)
+
+plt.show()
+
+fig = plt.figure(figsize=(10,8))
+plt.scatter(tocluster[1],tocluster[0],c = c_preds)
+for ci,c in enumerate(centers):
+    plt.plot(c[1], c[0], 'o', markersize=8, color='red', alpha=1)
+
+plt.xlabel('x_values')
+plt.ylabel('y_values')
+
+plt.title('Data points in 2D PCA axis', fontsize=20)
+plt.show()
+
+user_anime['cluster'] = c_preds
+
+user_anime.head(10)
+user_anime.info()
+
+c0 = user_anime[user_anime['cluster']==0].drop('cluster',axis=1).mean()
+c1 = user_anime[user_anime['cluster']==1].drop('cluster',axis=1).mean()
+c2 = user_anime[user_anime['cluster']==2].drop('cluster',axis=1).mean()
+c3 = user_anime[user_anime['cluster']==3].drop('cluster',axis=1).mean()
+
+c0.sort_values(ascending=False)[0:15]
+
+def createAnimeInfoList(animelist):
+    episode_list = list()
+    genre_list = list()
+    member_list = list()
+    rating_list= list()
+    for x in anime['name']:
+        if x in animelist:
+            episode_list.append(anime[anime['name']==x].episodes.values.astype(int))
+            member_list.append(anime[anime['name']==x].members.values.astype(int))
+            rating_list.append(anime[anime['name']==x].rating.values.astype(int))
+            for y in anime[anime['name']==x].genre.values:
+                 genre_list.append(y)
+    return genre_list,episode_list,rating_list,member_list
+
+def count_word(df, ref_col, liste):
+    keyword_count = dict()
+    for s in liste: keyword_count[s] = 0
+    for liste_keywords in df[ref_col].str.split(','):        
+        if type(liste_keywords) == float and pd.isnull(liste_keywords): continue        
+        for s in [s for s in liste_keywords if s in liste]: 
+            if pd.notnull(s): keyword_count[s] += 1
+    #______________________________________________________________________
+    # convert the dictionary in a list to sort the keywords by frequency
+    keyword_occurences = []
+    for k,v in keyword_count.items():
+        keyword_occurences.append([k,v])
+    keyword_occurences.sort(key = lambda x:x[1], reverse = True)
+    return keyword_occurences, keyword_count
+
+animelist = list(c0.index)
+data = pd.DataFrame()
+data['genre'],data['episode'],data['rating'],data['member'] =  createAnimeInfoList(animelist)
+
+set_keywords = set()
+for liste_keywords in data['genre'].str.split(',').values:
+    if isinstance(liste_keywords, float): continue  # only happen if liste_keywords = NaN
+    set_keywords = set_keywords.union(liste_keywords)
+
+print(c0.sort_values(ascending=False)[0:15])
+c0_animelist = list(c0.sort_values(ascending=False)[0:15].index)
+c0_data = pd.DataFrame()
+c0_data['genre'],c0_data['episode'],c0_data['rating'],c0_data['member'] =  createAnimeInfoList(c0_animelist)
+c0_data.iloc[:,1:4] = c0_data.iloc[:,1:4].astype(int) # change to numeric object to integer
+keyword_occurences, dum = count_word(c0_data, 'genre', set_keywords)
+
+#Popular genres
+print(keyword_occurences[0:5])
+
+#Average of each information for anime which user in this cluster like
+#Cluster 0
+print('cluster 0\nAVG episode : {0}\nAVG movie rating : {1}\nAVG member : {2}'
+      .format(c0_data['episode'].mean(), c0_data['rating'].mean(),c0_data['member'].mean()))
+
+#cluster 1
+print(c1.sort_values(ascending=False)[0:15])
+c1_animelist = list(c1.sort_values(ascending=False)[0:15].index)
+c1_data = pd.DataFrame()
+c1_data['genre'],c1_data['episode'],c1_data['rating'],c1_data['member'] =  createAnimeInfoList(c1_animelist)
+c1_data.iloc[:,1:4] = c1_data.iloc[:,1:4].astype(int)
+keyword_occurences, dum = count_word(c1_data, 'genre', set_keywords)
+print(keyword_occurences[0:5])
+print('cluster 1\nAVG episode : {0}\nAVG movie rating : {1}\nAVG member : {2}'
+      .format(c1_data['episode'].mean(), c1_data['rating'].mean(),c1_data['member'].mean()))
+
+#cluster 2
+print(c2.sort_values(ascending=False)[0:15])
+c2_animelist = list(c2.sort_values(ascending=False)[0:15].index)
+c2_data = pd.DataFrame()
+c2_data['genre'],c2_data['episode'],c2_data['rating'],c2_data['member'] =  createAnimeInfoList(c2_animelist)
+c2_data.iloc[:,1:4] = c2_data.iloc[:,1:4].astype(int)
+keyword_occurences, dum = count_word(c2_data, 'genre', set_keywords)
+print(keyword_occurences[0:5])
+print('cluster 2\nAVG episode : {0}\nAVG movie rating : {1}\nAVG member : {2}'
+      .format(c2_data['episode'].mean(), c2_data['rating'].mean(),c2_data['member'].mean()))
+
+#cluster 3
+print(c3.sort_values(ascending=False)[0:15])
+c3_animelist = list(c3.sort_values(ascending=False)[0:15].index)
+c3_data = pd.DataFrame()
+c3_data['genre'],c3_data['episode'],c3_data['rating'],c3_data['member'] =  createAnimeInfoList(c3_animelist)
+c3_data.iloc[:,1:4] = c3_data.iloc[:,1:4].astype(int)
+keyword_occurences, dum = count_word(c3_data, 'genre', set_keywords)
+print(keyword_occurences[0:5])
+print('cluster 3\nAVG episode : {0}\nAVG movie rating : {1}\nAVG member : {2}'
+      .format(c3_data['episode'].mean(), c3_data['rating'].mean(),c3_data['member'].mean()))
